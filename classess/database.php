71477dcb1b96470c1c323319790a5b2493f9de5b
Timothy Baracael <baracaelgm@students.nu-lipa.edu.ph>
2025-05-16 14:33:28 +0800

    Updated

--- classess/database.php
+++ classess/database.php
@@ -1,26 +1,41 @@
 <?php
  
 class database{
+ 
     function opencon(): PDO{
         return new PDO(
-            dsn: 'mysql:host=localhost;dbname=dbs_app_v2',
+            dsn: 'mysql:host=localhost;
+            dbname=dbs_app_v2',
             username: 'root',
-            password: '');
+            password: ''
+        );
     }
-    function signupUser($firstname, $lastname, $username, $password){
+    function signupUser($firstname,$lastname,$username,$password){
         $con = $this->opencon();
         try{
-            $con->beginTransaction();
- 
-            $stmt = $con->prepare("INSERT INTO Admin (admin_FN, admin_LN, admin_username, admin_password) VALUES (?, ?, ?, ?)");
-            $stmt->execute([$firstname, $lastname, $username, $password]);
-            $userID = $con->lastInsertID();
-            $con->commit();
+            $con-> beginTransaction();
+             $stmt=$con->prepare("INSERT INTO Admin(
+            admin_FN,
+            admin_LN,
+            admin_username,
+            admin_password)VALUES(?,?,?,?)");
+            $stmt->execute([$firstname,$lastname,$username,$password]);
+            $userID = $con->lastInsertId();
+            $con-> commit();
             return $userID;
-        } catch (PDOException $e) {
-            $con->rollback();
+        } catch (PDOException $E){
+            $con-> rollBack();
             return false;
         }
  
     }
+    function isUsernameExists($username){
+        $con = $this->opencon();
+        $stmt = $con->prepare("SELECT * FROM Admin WHERE admin_username = ?");
+        $stmt->execute([$username]);
+        $count = $stmt->fetchColumn();
+ 
+        return $count > 0;
+    }
+   
 }
